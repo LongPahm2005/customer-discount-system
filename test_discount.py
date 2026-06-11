a7dcc3f298aa598b854a405d25ae6b585ab963bc
Pham Van Long <phamvanlong050505@gmail.com>
2026-06-11 15:46:09 +0700

    update test case

--- test_discount.py
+++ test_discount.py
@@ -1,13 +1,10 @@
 from discount import giamgia
 
 def test_giamgia():
-    assert giamgia(50000000, 0) == 0.1
+    assert giamgia(60000000, 2000000) == 0.1
 
 def test_giamgia_2():
-    assert giamgia(25000000, 25000000) == 0.1
+    assert giamgia(30000000, 2000000) == 0
 
 def test_giamgia_3():
-    assert giamgia(3000000,0) == 0
-
-def test_giamgia_4():
-    assert giamgia(0,3000000) == 0
+    assert giamgia(49000000,2000000) == 0.1
